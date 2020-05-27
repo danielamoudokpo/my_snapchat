42d1ca03bc6d3b4e5ff563cacf60c98f30329946
danielamoudokpo <62663366+danielamoudokpo@users.noreply.github.com>
2020-05-27 05:45:51 +0200

    client

---
+++ client/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>he</Text>
+      <Image source={{uri:'./assets/snap2.png'}}/>
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
